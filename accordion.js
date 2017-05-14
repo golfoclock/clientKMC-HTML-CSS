0f0d03a1953134739b20cd6ca66aa130ef2d3333
golfoclock <cmmc@protonmail.ch>
2017-05-14 18:02:03 -0400

    added JS/jQuery files

---
+++ accordion.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	"use strict";
+	
+
+	$('.accordion .accordion-control').on('click', function(e){
+		e.preventDefault();
+console.log('click')
+
+		$(this)
+		.next('.accordion-panel')
+		.slideToggle();
+
+	});	// end accordion click
+
+
+});	//end ready
